4d9343a0e8d9d942cfbc20a71c4b6c289c51898d
Chris Ricci <chris.ricci@coreos.com>
2019-01-28 13:06:19 -0500

    Adding random name selection

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #!flask/bin/python
 from flask import Flask
 from flask_prometheus import monitor 
+from random
 
 app = Flask(__name__)
 
+names = open('names.txt', 'r')
+
 @app.route('/')
 def index():
-    return "Chris"
+    return random.choice(names)
 
 if __name__ == '__main__':
     monitor(app, port=8000)
